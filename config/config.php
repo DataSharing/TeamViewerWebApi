85d8bed66f4ecea13190d20d8ea27179fc4eefbd
HEDDAJI Walid <walid.heddaji@univ-lyon3.fr>
2018-07-20 12:21:59 +0200

    Up src

--- config/config.php
+++ config/config.php
@@ -7,8 +7,10 @@ Class Config{
 	Public $redirect_uri;
 	Public $debug = false;
 	Public $debug_http = true;
+	Public $base_url;
 
 	public function __construct(){
 		$this->redirect_uri  = 'http://'.$_SERVER['HTTP_HOST'].dirname(strtok($_SERVER['REQUEST_URI'],'?'))."/login.php";
+		$this->base_url = 'http://'.$_SERVER['HTTP_HOST'].dirname(strtok($_SERVER['REQUEST_URI'],'?'));
 	}	
 }
